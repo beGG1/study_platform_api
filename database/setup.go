1b0c6f9bb0f743a7e106886d6fd687358d2688f6
Stepan Shabalinov <shabalinov2341234@gmail.com>
2024-10-02 18:16:17 +0300

    ....

--- database/setup.go
+++ database/setup.go
@@ -1,3 +1,22 @@
 package database
 
+import (
+	"database/sql"
 
+	_ "github.com/lib/pq"
+)
+
+func InitDB() *sql.DB {
+	connStr := "user=admin dbname=platform password=admin sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/beGG1/study_platform_api/database"
 	"github.com/beGG1/study_platform_api/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	database.InitDB()
 	r := gin.Default()
 	routes.SetupRoutes(r)
 	r.Run() // listen and serve on 0.0.0.0:8080
